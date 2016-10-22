054a6a358692728a6a206ff5a279b86fe7f7c3a0
Alexandr Yakovets <375296904276@mail.ru>
2016-10-22 14:59:39 +0300

    Developing task №2.Added dll and Wpf-application

---
+++ Task_2/Task_2/MyMath/AlternativeMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    public class AlternativeMath
+    {
+        static public double SqrtByNewton(double n, double A, double eps)
+        {
+            var x0 = A / n;
+            var x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+
+            while (Math.Abs(x1 - x0) > eps)
+            {
+                x0 = x1;
+                x1 = (1 / n) * ((n - 1) * x0 + A / Math.Pow(x0, (int)n - 1));
+            }
+            return x1;
+        }
+
+    }
+}
---
+++ Task_2/Task_2/Task_2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyMath;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double n, A, eps,result;
+            try
+            {
+                if (Double.TryParse(inputN.Text, out n) == false) new FormatException();
+                if (Double.TryParse(inputA.Text, out A) == false) new FormatException();
+                if (Double.TryParse(inputEps.Text, out eps) == false) new FormatException();
+                result=AlternativeMath.SqrtByNewton(n, A, eps);
+                richTextBox.AppendText(result + "\n");
+            }
+            catch (FormatException)
+            {
+                richTextBox.AppendText("input data is not numeral");
+            }
+
+
+        }
+    }
+}
