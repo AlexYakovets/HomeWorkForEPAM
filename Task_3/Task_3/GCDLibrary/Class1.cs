465c29742c97906426e241131f7c7b4ac276f95d
Alexander Yakovets <375296904276@mail.ru>
2016-10-24 10:53:57 +0300

    added library for work with GDC

---
+++ Task_3/Task_3/GCDLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDLibrary
+{
+    public class GCDAlgoritms
+    {
+        public static int GCDByEuclide(int number1, int number2)
+        {
+            int Gcd=0;
+            return Gcd;
+        }
+
+
+
+    }
+}
