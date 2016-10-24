f0a1419e9ab4345704d3a7a402a044ec4e4f246d
Alexander Yakovets <375296904276@mail.ru>
2016-10-24 11:51:54 +0300

    Added metod "GCDByEuclide".Renamed class Class1 to GCDAlgoritms

---
+++ Task_3/Task_3/GCDLibrary/GCDAlgoritms.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDLibrary
+{
+    public class GCDAlgoritms
+    {
+        public static int GCDByEuclide(int number1, int number2)
+        {
+            int maxnumber;
+            int balance;
+            if (number1 == number2) return number1;
+            else
+            {
+                if (number1 < number2)
+                {
+                    number1 = +number2;
+                    number2 = number1 - number2;
+                    number1 = -number2;
+                }
+                maxnumber = number1;
+                balance = number2;
+                while (balance != 0)
+                {
+                    while (maxnumber > balance)
+                    {
+                        maxnumber -= balance;
+                    }
+                    balance += maxnumber;
+                    maxnumber = balance - maxnumber;
+                    balance = -maxnumber;
+                }
+
+            }
+            return maxnumber;
+        }
+
+
+
+    }
+}
