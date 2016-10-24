cd97f96fae988d5995717707c203a2d0e160244c
Alexandr Yakovets <375296904276@mail.ru>
2016-10-24 10:24:45 +0300

    Created Task_3

---
+++ Task_3/Task_3/ClassLibrary2/Algoritms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAlgoritms
+{
+    public class Algoritms
+    {
+        public static int GCDByEuclid(int number1, int number2)
+        {
+
+            return GCD;
+        }
+    }
+}
