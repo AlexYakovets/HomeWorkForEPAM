7d5e994de65ffb302dd8e081868ba07e1d7dbdc0
Alexander Yakovets <375296904276@mail.ru>
2016-10-26 11:09:08 +0300

    added unit tests for metods in GCDAlgoritms

---
+++ Task_3/Task_3/UnitTests/UnitTestGCDAlgoritms.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GCDLibrary;
+
+namespace UnitTestGCDAlgoritms
+{
+    [TestClass]
+    public class UnitGCDAlgoritmsTests
+    {
+        [TestMethod]
+        public void GCDByEuclide_GCDof775and225is75_75()
+        {
+            // arrange
+            long dummy;
+            int number1 = 750;
+            int number2 = 225;
+            // act
+            int result = GCDAlgoritms.GCDByEuclide(number1,number2, out dummy);
+            // assert
+            Assert.AreEqual(75, result);
+        }
+        [TestMethod]
+        public void GDCByStein_GCDof775and225is75_75()
+        {
+            // arrange
+            long dummy;
+            int number1 = 750;
+            int number2 = 225;
+            // act
+            int result = GCDAlgoritms.GCDByStein(number1, number2, out dummy);
+            // assert
+            Assert.AreEqual(75, result);
+        }
+    }
+}
