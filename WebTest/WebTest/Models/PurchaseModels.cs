59213cdd383a0cc094de33dea8c75d68daeb66fb
Alexander Yakovets <375296904276@mail.ru>
2016-11-17 20:52:16 +0300

    added WebTest project

---
+++ WebTest/WebTest/Models/PurchaseModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebTest.Models
+{
+    public class PurchaseModels
+    {
+        // ID покупки
+        [Key]
+        public int PurchaseId { get; set; }
+        // имя и фамилия покупателя
+        public string Person { get; set; }
+        // адрес покупателя
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        // дата покупки
+        public DateTime Date { get; set; }
+    }
+}
